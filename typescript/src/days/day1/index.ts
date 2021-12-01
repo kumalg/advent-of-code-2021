d850f019ce952ef4a94594f7e32c9d6885467870
Kamil Golec <kumalgfilms@gmail.com>
2021-12-01 06:38:40 +0100

    day 1

--- typescript/src/days/day1/index.ts
+++ typescript/src/days/day1/index.ts
@@ -2,21 +2,48 @@ import { readFileSync } from "fs";
 import { Day } from "interfaces/day";
 import path from "path/posix";
 
+interface Acc {
+  previous: null | number;
+  increasedCount: number;
+}
+
 export default class Day1 implements Day {
-  input: Array<string>;
+  input: Array<number>;
 
   constructor() {
     this.input = readFileSync(path.join(__dirname, "input.txt"), "utf-8")
       .split("\r\n")
-      .filter((line) => line);
+      .filter((line) => line)
+      .map((line) => parseInt(line));
+  }
+
+  private getIncreasedCount(array: Array<number>): number {
+    return array.reduce<Acc>(
+      ({ previous, increasedCount }, current) => {
+        return {
+          previous: current,
+          increasedCount:
+            previous !== null && previous < current
+              ? increasedCount + 1
+              : increasedCount,
+        };
+      },
+      {
+        previous: null,
+        increasedCount: 0,
+      }
+    ).increasedCount;
   }
 
   async firstPart(): Promise<string | number> {
-    console.log(this.input);
-    return "mam to";
+    return this.getIncreasedCount(this.input);
   }
 
   async secondPart(): Promise<string | number> {
-    return "mam to 2";
+    const list = this.input
+      .slice(0, -2)
+      .map((number, i) => number + this.input[i + 1] + this.input[i + 2]);
+
+    return this.getIncreasedCount(list);
   }
 }
