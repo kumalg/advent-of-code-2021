e69d0de378b23d38aac16d1e4d2f067a5b35fed0
Kamil Golec <kamil.golec@eobuwie.com.pl>
2021-12-01 13:01:33 +0100

    better console

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
+import { bold, yellowBright } from "chalk";
 import Day from "./days/day01";
+
+function formattedResult(result: string | number): string {
+  return bold(yellowBright(result));
+}
 
 export const main = (): boolean => {
   const day = new Day();
   const arg = process.argv[2];
 
   if (!arg || arg === "1") {
-    console.log(`First star: ${day.firstStar()}`);
+    console.log(`First star:  ${formattedResult(day.firstStar())}`);
   }
 
   if (!arg || arg === "2") {
-    console.log(`Second star: ${day.secondStar()}`);
+    console.log(`Second star: ${formattedResult(day.secondStar())}`);
   }
 
   return true;
