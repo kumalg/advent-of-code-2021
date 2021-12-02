488e07658f915109f20c4461df5fede0f71b0f31
Kamil Golec <kumalgfilms@gmail.com>
2021-12-02 08:23:36 +0100

    day 2

--- typescript/src/days/day02.ts
+++ typescript/src/days/day02.ts
@@ -1,18 +1,53 @@
 import { Day } from "../day";
 
 export default class extends Day {
-  input: Array<string>;
+  input: Array<{ type: string; value: number }>;
 
   constructor(resourcesPath: string) {
     super(resourcesPath, __filename);
-    this.input = this.getInputLines();
+    this.input = this.getInputLines("input.txt").map((line) => {
+      const [type, value] = line.split(" ");
+      return {
+        type,
+        value: parseInt(value),
+      };
+    });
   }
 
-  firstStar(): string | number {
-    throw new Error("Method not implemented.");
+  firstStar(): number {
+    const finalPosition = this.input.reduce(
+      (position, { type, value }) => {
+        if (type === "forward") {
+          position.horizontal += value;
+        } else if (type === "down") {
+          position.depth += value;
+        } else if (type === "up") {
+          position.depth -= value;
+        }
+        return position;
+      },
+      { horizontal: 0, depth: 0 }
+    );
+
+    return finalPosition.horizontal * finalPosition.depth;
   }
 
-  secondStar(): string | number {
-    throw new Error("Method not implemented.");
+  secondStar(): number {
+    const finalPosition = this.input.reduce(
+      (position, { type, value }) => {
+        if (type === "forward") {
+          position.horizontal += value;
+          position.depth += position.aim * value;
+        } else if (type === "down") {
+          position.aim += value;
+        } else if (type === "up") {
+          position.aim -= value;
+        }
+        return position;
+      },
+      { aim: 0, horizontal: 0, depth: 0 }
+    );
+
+    return finalPosition.horizontal * finalPosition.depth;
   }
 }
