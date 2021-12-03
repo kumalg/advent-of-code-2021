04497ee0e3fa0c167656713686b643189e596e18
Kamil Golec <kumalgfilms@gmail.com>
2021-12-03 06:38:15 +0100

    less code

--- typescript/src/days/day03.ts
+++ typescript/src/days/day03.ts
@@ -31,29 +31,15 @@ export default class extends Day {
     let scrubber = [...this.input];
 
     for (let i = 0; i < width && oxygen.length > 1; i++) {
-      const oxygenDigits = groupBy(oxygen.map((line) => line[i]));
-
-      const oxygenLength0 = oxygenDigits["0"]?.length || 0;
-      const oxygenLength1 = oxygenDigits["1"]?.length || 0;
-
-      if (oxygenLength0 > oxygenLength1) {
-        oxygen = oxygen.filter((o) => o[i] === "0");
-      } else {
-        oxygen = oxygen.filter((o) => o[i] === "1");
-      }
+      const digits = groupBy(oxygen.map((line) => line[i]));
+      const remaining = digits["0"].length > digits["1"].length ? "0" : "1";
+      oxygen = oxygen.filter((o) => o[i] === remaining);
     }
 
     for (let i = 0; i < width && scrubber.length > 1; i++) {
-      const scrubberDigits = groupBy(scrubber.map((line) => line[i]));
-
-      const scrubberLength0 = scrubberDigits["0"].length || 0;
-      const scrubberLength1 = scrubberDigits["1"].length || 0;
-
-      if (scrubberLength0 > scrubberLength1) {
-        scrubber = scrubber.filter((o) => o[i] === "1");
-      } else {
-        scrubber = scrubber.filter((o) => o[i] === "0");
-      }
+      const digits = groupBy(scrubber.map((line) => line[i]));
+      const remaining = digits["0"].length > digits["1"].length ? "1" : "0";
+      scrubber = scrubber.filter((o) => o[i] === remaining);
     }
 
     return parseInt(oxygen[0], 2) * parseInt(scrubber[0], 2);
