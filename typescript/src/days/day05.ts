2227a5b1a84752ecafb8ffc8e029511ffad78289
Kamil Golec <kumalgfilms@gmail.com>
2021-12-05 11:53:48 +0100

    refactor

--- typescript/src/days/day05.ts
+++ typescript/src/days/day05.ts
@@ -18,7 +18,7 @@ export default class extends Day {
     });
   }
 
-  firstStar(): number {
+  private getOverlappedPointsCount(withDiagonals: boolean): number {
     const points = this.input
       .map(({ start, end }) => {
         if (start[0] === end[0]) {
@@ -35,32 +35,10 @@ export default class extends Day {
           ]);
         }
 
-        return [];
-      })
-      .flatMap((e) => e)
-      .map((eee) => eee[0] + "," + eee[1]);
-
-    return Object.values(groupBy(points)).filter((e) => e.length > 1).length;
-  }
-
-  secondStar(): number {
-    const points = this.input
-      .map(({ start, end }) => {
-        if (start[0] === end[0]) {
-          return [...new Array(Math.abs(end[1] - start[1]) + 1)].map((_, i) => [
-            end[0],
-            Math.min(end[1], start[1]) + i,
-          ]);
-        }
-
-        if (start[1] === end[1]) {
-          return [...new Array(Math.abs(end[0] - start[0]) + 1)].map((_, i) => [
-            Math.min(end[0], start[0]) + i,
-            end[1],
-          ]);
-        }
-
-        if (Math.abs(start[0] - end[0]) === Math.abs(start[1] - end[1])) {
+        if (
+          withDiagonals &&
+          Math.abs(start[0] - end[0]) === Math.abs(start[1] - end[1])
+        ) {
           return [...new Array(Math.abs(end[0] - start[0]) + 1)].map((_, i) => [
             start[0] < end[0] ? start[0] + i : start[0] - i,
             start[1] < end[1] ? start[1] + i : start[1] - i,
@@ -73,5 +51,13 @@ export default class extends Day {
       .map((eee) => eee[0] + "," + eee[1]);
 
     return Object.values(groupBy(points)).filter((e) => e.length > 1).length;
+  }
+
+  firstStar(): number {
+    return this.getOverlappedPointsCount(false);
+  }
+
+  secondStar(): number {
+    return this.getOverlappedPointsCount(true);
   }
 }
