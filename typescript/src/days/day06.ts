a9a6730026acbcb8626d3378e1f8bcc21b7be99d
Kamil Golec <kumalgfilms@gmail.com>
2021-12-06 23:56:20 +0100

    less code

--- typescript/src/days/day06.ts
+++ typescript/src/days/day06.ts
@@ -1,8 +1,6 @@
 import { groupBy } from "lodash";
 import { Day } from "../day";
 
-// "test.txt"
-
 export default class extends Day {
   input: Array<number>;
 
@@ -15,28 +13,15 @@ export default class extends Day {
   }
 
   countLanternfishes(days: number) {
-    const list = [...this.input];
-
-    const listObject = Object.fromEntries(
-      Object.entries(groupBy(list)).map(([key, value]) => [
-        key.toString(),
-        value.length,
-      ])
+    const groupedTimers = groupBy(this.input);
+    const initialState = [...new Array(9)].map(
+      (_, i) => groupedTimers[i.toString()]?.length || 0
     );
 
-    const result: object = [...new Array(days)].reduce((acc) => {
-      return {
-        0: acc["1"] || 0,
-        1: acc["2"] || 0,
-        2: acc["3"] || 0,
-        3: acc["4"] || 0,
-        4: acc["5"] || 0,
-        5: acc["6"] || 0,
-        6: (acc["7"] || 0) + (acc["0"] || 0),
-        7: acc["8"] || 0,
-        8: acc["0"] || 0,
-      };
-    }, listObject);
+    const result: object = [...new Array(days)].reduce(
+      (acc) => [...acc.slice(1, 7), acc[7] + acc[0], acc[8], acc[0]],
+      initialState
+    );
 
     return Object.values(result).reduce((acc, curr) => acc + curr, 0);
   }
