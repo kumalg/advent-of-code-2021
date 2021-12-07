bb210481778de196c07dd30d8dc46d33113bf37c
Kamil Golec <kumalgfilms@gmail.com>
2021-12-07 01:03:59 +0100

    cleaning

--- typescript/src/days/day06.ts
+++ typescript/src/days/day06.ts
@@ -15,9 +15,7 @@ export default class extends Day {
 
   countLanternfishes(days: number) {
     const groupedTimers = groupBy(this.input);
-    const initialState: Array<number> = range(9).map(
-      (i) => groupedTimers[i]?.length || 0
-    );
+    const initialState = range(9).map((i) => groupedTimers[i]?.length || 0);
 
     return range(days)
       .reduce(
